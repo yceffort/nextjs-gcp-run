78d51ce7ada9950d2be5be7dfca329254e6e4f21
yceffort <yc.effort@navercorp.com>
2022-09-23 18:09:40 +0900

    :memo: add useEffect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  
+  useEffect(() => {
+    console.log("hello!")
+  })
+  
   return (
     <div className={styles.container}>
       <Head>
